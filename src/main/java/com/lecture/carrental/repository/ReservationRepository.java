8553da23b19c7aee046e78cb7a509fb6b90e1640
hsnbsry <basriakbas@hotmail.com>
2022-03-02 19:07:16 +0100

    02Mar

--- src/main/java/com/lecture/carrental/repository/ReservationRepository.java
+++ src/main/java/com/lecture/carrental/repository/ReservationRepository.java
@@ -1,7 +1,10 @@
 package com.lecture.carrental.repository;
 
 import com.lecture.carrental.domain.Reservation;
+import com.lecture.carrental.domain.User;
 import com.lecture.carrental.domain.enumeration.ReservationStatus;
+import com.lecture.carrental.dto.ReservationDTO;
+import com.lecture.carrental.exception.ResourceNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,10 +12,22 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @Transactional
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    //    @Query("SELECT new com.lecture.carrental.dto.ReservationDTO(r) FROM Reservation r WHERE r.userId = ?1")
+    List<ReservationDTO> findAllByUserId(User userId);
+
+    //    @Query("SELECT new com.lecture.carrental.dto.ReservationDTO(r) FROM Reservation r WHERE r.id = ?1 and r.userId = ?2")
+    Optional<ReservationDTO> findByIdAndUserId(Long id, User user) throws ResourceNotFoundException;
+
+    //    @Query("SELECT new com.lecture.carrental.dto.ReservationDTO(r) FROM Reservation r")
+    List<ReservationDTO> findAllBy();
+
+    Optional<ReservationDTO> findByIdOrderById(Long id) throws ResourceNotFoundException;
 
     @Query("SELECT r FROM Reservation r " +
             "LEFT JOIN fetch r.carId cd " +
